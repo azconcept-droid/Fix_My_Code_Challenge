0d3e4c4b266faa7379353df8e38485e197958308
Yahaya <azconcept2016@gmail.com>
2022-10-11 00:37:42 +0100

    fix: fix perimeter formular

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""This module declare class square"""
+
 
 class square():
-    
+    """A declaration of square class"""
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initialization"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,15 +17,18 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def PerimeterOfMySquare(self):
+        """Perimeter of the square"""
+        return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
+        """Return square object"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimeterOfMySquare())
